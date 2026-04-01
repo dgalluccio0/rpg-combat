55a9365710adec6fe57f7de5d93faa2b347a5614
dgalluccio0 <daniele.galluccio0@gmail.com>
2026-04-01 12:36:50 +0200

    disabled csrf

--- src/main/java/io/github/dgalluccio0/rpgcombat/config/SecurityConfig.java
+++ src/main/java/io/github/dgalluccio0/rpgcombat/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+			.csrf(csrf -> csrf.disable()) // disabilitato perché userò i token JWT
 			.authorizeHttpRequests(authorize ->
 				authorize.anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults());
